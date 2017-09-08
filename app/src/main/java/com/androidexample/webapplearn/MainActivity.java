2a07d9bdc0afa96d8158c30380f69ab411bc073c
ANSHUL YADAV <anshulyadav1911@gmail.com>
2017-09-09 00:44:07 +0530

    Initial commit

---
+++ app/src/main/java/com/androidexample/webapplearn/MainActivity.java
@@ -0,0 +1,25 @@
+package com.androidexample.webapplearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://iittp.ac.in/");
+    }
+
+
+}
